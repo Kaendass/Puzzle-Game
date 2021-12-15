1c2b959b97df9c550bebff5736614d3d9fd25220
Tuna Kokarcalı <77245226+Kaendass@users.noreply.github.com>
2021-12-15 19:32:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,58 +18,68 @@ function placement() {
 	}
 }
 var move_counter = 0;
-
+var enable = true;
 function move(row, col) {
-	var top = row - 1;
-	var bottom = row + 1;
-	var left = col - 1;
-	var right = col + 1;
+	if (enable) {
+		var top = row - 1;
+		var bottom = row + 1;
+		var left = col - 1;
+		var right = col + 1;
 
-	if (row != 0 && document.getElementById('cell' + top + col).innerHTML == '') {
-		var cell_value = document.getElementById('cell' + row + col).innerHTML;
-		var new_cell_value = document.getElementById('cell' + top + col).innerHTML;
-		document.getElementById('cell' + top + col).innerHTML = cell_value;
-		document.getElementById('cell' + row + col).innerHTML = new_cell_value;
-		move_counter++;
-		document.getElementById('move_count').innerHTML =
-			'Number of Moves: ' + move_counter;
-	} else if (
-		row != 3 &&
-		document.getElementById('cell' + bottom + col).innerHTML == ''
-	) {
-		var cell_value = document.getElementById('cell' + row + col).innerHTML;
-		var new_cell_value = document.getElementById(
-			'cell' + bottom + col
-		).innerHTML;
-		document.getElementById('cell' + bottom + col).innerHTML = cell_value;
-		document.getElementById('cell' + row + col).innerHTML = new_cell_value;
-		move_counter++;
-		document.getElementById('move_count').innerHTML =
-			'Number of Moves: ' + move_counter;
-	} else if (
-		col != 0 &&
-		document.getElementById('cell' + row + left).innerHTML == ''
-	) {
-		var cell_value = document.getElementById('cell' + row + col).innerHTML;
-		var new_cell_value = document.getElementById('cell' + row + left).innerHTML;
-		document.getElementById('cell' + row + left).innerHTML = cell_value;
-		document.getElementById('cell' + row + col).innerHTML = new_cell_value;
-		move_counter++;
-		document.getElementById('move_count').innerHTML =
-			'Number of Moves: ' + move_counter;
-	} else if (
-		col != 3 &&
-		document.getElementById('cell' + row + right).innerHTML == ''
-	) {
-		var cell_value = document.getElementById('cell' + row + col).innerHTML;
-		var new_cell_value = document.getElementById(
-			'cell' + row + right
-		).innerHTML;
-		document.getElementById('cell' + row + right).innerHTML = cell_value;
-		document.getElementById('cell' + row + col).innerHTML = new_cell_value;
-		move_counter++;
-		document.getElementById('move_count').innerHTML =
-			'Number of Moves: ' + move_counter;
+		if (
+			row != 0 &&
+			document.getElementById('cell' + top + col).innerHTML == ''
+		) {
+			var cell_value = document.getElementById('cell' + row + col).innerHTML;
+			var new_cell_value = document.getElementById(
+				'cell' + top + col
+			).innerHTML;
+			document.getElementById('cell' + top + col).innerHTML = cell_value;
+			document.getElementById('cell' + row + col).innerHTML = new_cell_value;
+			move_counter++;
+			document.getElementById('move_count').innerHTML =
+				'Number of Moves: ' + move_counter;
+		} else if (
+			row != 3 &&
+			document.getElementById('cell' + bottom + col).innerHTML == ''
+		) {
+			var cell_value = document.getElementById('cell' + row + col).innerHTML;
+			var new_cell_value = document.getElementById(
+				'cell' + bottom + col
+			).innerHTML;
+			document.getElementById('cell' + bottom + col).innerHTML = cell_value;
+			document.getElementById('cell' + row + col).innerHTML = new_cell_value;
+			move_counter++;
+			document.getElementById('move_count').innerHTML =
+				'Number of Moves: ' + move_counter;
+		} else if (
+			col != 0 &&
+			document.getElementById('cell' + row + left).innerHTML == ''
+		) {
+			var cell_value = document.getElementById('cell' + row + col).innerHTML;
+			var new_cell_value = document.getElementById(
+				'cell' + row + left
+			).innerHTML;
+			document.getElementById('cell' + row + left).innerHTML = cell_value;
+			document.getElementById('cell' + row + col).innerHTML = new_cell_value;
+			move_counter++;
+			document.getElementById('move_count').innerHTML =
+				'Number of Moves: ' + move_counter;
+		} else if (
+			col != 3 &&
+			document.getElementById('cell' + row + right).innerHTML == ''
+		) {
+			var cell_value = document.getElementById('cell' + row + col).innerHTML;
+			var new_cell_value = document.getElementById(
+				'cell' + row + right
+			).innerHTML;
+			document.getElementById('cell' + row + right).innerHTML = cell_value;
+			document.getElementById('cell' + row + col).innerHTML = new_cell_value;
+			move_counter++;
+			document.getElementById('move_count').innerHTML =
+				'Number of Moves: ' + move_counter;
+		}
+		finish();
 	}
 }
 
@@ -78,6 +88,32 @@ function reset() {
 	document.getElementById('move_count').innerHTML =
 		'Number of Moves: ' + move_counter;
 	placement();
+	enable = true;
+	document.getElementById('welcome').innerHTML = 'Puzzle Game';
 }
 
+counter = 0;
+function finish() {
+	for (i = 0; i < 4; i++) {
+		if (document.getElementById('cell0' + i).innerHTML == i + 1) {
+			counter++;
+		}
+		if (document.getElementById('cell1' + i).innerHTML == i + 5) {
+			counter++;
+		}
+		if (document.getElementById('cell2' + i).innerHTML == i + 9) {
+			counter++;
+		}
+		if (document.getElementById('cell3' + i).innerHTML == i + 13) {
+			counter++;
+		}
+	}
+	if (counter == 15) {
+		document.getElementById('welcome').innerHTML = 'You win!';
+		enable = false;
+	}
+	console.log(counter);
+	counter = 0;
+}
 placement();
+finish();
